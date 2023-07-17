66f76a1cd1b123353cdea0e661e90377b58a2fa2
DD <deepakdhake99@gmail.com>
2023-07-17 15:37:22 +0530

    New Employee Web App

---
+++ DOTNET/EmployeeWebApp/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace EmployeeWebApp.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public double salary { get; set; }
+        public string city { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
